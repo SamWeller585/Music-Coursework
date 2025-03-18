5852b2a7cbdca162621bd10d1a0630d88995a0e5
SaamWeller585 <sammybweller@gmail.com>
2025-03-18 22:02:48 +0000

    Main file update for User interaction :D

--- Main.java
+++ Main.java
@@ -1,3 +1,61 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        MusicLibrary library = new MusicLibrary();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nMusic Library Menu:");
+            System.out.println("1. Add a new song");
+            System.out.println("2. Remove a song");
+            System.out.println("3. Print all songs");
+            System.out.println("4. Print songs by play count");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter song title: ");
+                    String title = scanner.nextLine();
+                    System.out.print("Enter artist name: ");
+                    String artist = scanner.nextLine();
+                    System.out.print("Enter play count: ");
+                    int playCount = scanner.nextInt();
+                    library.addSong(title, artist, playCount);
+                    break;
+
+                case 2:
+                    System.out.print("Enter the song title to remove: ");
+                    String removeTitle = scanner.nextLine();
+                    library.removeSong(removeTitle);
+                    break;
+
+                case 3:
+                    System.out.println("\nAll Songs:");
+                    library.printAllSongs();
+                    break;
+
+                case 4:
+                    System.out.print("Enter minimum play count: ");
+                    int minPlays = scanner.nextInt();
+                    System.out.println("\nSongs with at least " + minPlays + " plays:");
+                    library.printSongsByPlayCount(minPlays);
+                    break;
+
+                case 5:
+                    running = false;
+                    System.out.println("Exiting program.");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        scanner.close();
+    }
 }
